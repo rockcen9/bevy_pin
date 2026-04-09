4eae9b99c89b08d929d9ebcdfeee10c2b4220258
rockcen <58832128+rockcen9@users.noreply.github.com>
2026-04-09 19:08:22 +0800

    remove unused code

--- src/prelude.rs
+++ src/prelude.rs
@@ -2,17 +2,16 @@ pub use crate::manager::connection::ConnectionState;
 pub use crate::ui_layout::body::SidebarState;
 #[allow(unused_imports)]
 pub use crate::{PausableSystems, Pause};
+#[allow(unused_imports)]
 pub use anyhow;
 pub use bevy::ecs::template::template;
 pub use bevy::platform::collections::{HashMap, HashSet};
 pub use bevy::prelude::*;
 pub use bevy::scene::prelude::Scene;
-#[allow(unused_imports)]
 pub use brp_helper::{
     BrpCommandsExt, BrpGetComponents, BrpGetResources, BrpHeartbeat, BrpListComponents,
-    BrpListResources, BrpMutate, BrpPlugin, BrpQueryEntry, BrpSchema, BrpSpawnEntity,
-    BrpWorldQuery, RpcResponse, TimeoutError,
+    BrpListResources, BrpMutate, BrpPlugin, BrpSchema, BrpSpawnEntity, BrpWorldQuery, RpcResponse,
+    TimeoutError,
 };
-pub use serde::Deserialize;
 pub use serde_json::{self, json};
 pub use webbrowser;
